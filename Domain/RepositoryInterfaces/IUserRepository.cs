98c796858eb49d756fdf8f0cb19340a0cffcf08c
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-11-06 21:38:28 +0200

    added initial repositoryInterfaces

---
+++ Domain/RepositoryInterfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+namespace Domain.RepositoryInterfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmailAsync(string email);
+        Task<bool> EmailExistsAsync(string email);
+        Task<IEnumerable<User>> GetUsersByRoleAsync(string role);
+        Task<bool> ValidateCredentialsAsync(string email, string password);
+    }
+
+
+}
