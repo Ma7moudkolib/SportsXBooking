38e57931b963c4cdaee329eb595ab5e0c1599602
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-11-24 00:02:58 +0200

    update main program file

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,10 +1,27 @@
+using Application.Mapping;
+using Application.ServiceInterfaces;
+using Presentation.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+builder.Services.AddAuthentication();
+builder.Services.ConfigureJWT(builder.Configuration);
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+if(app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
