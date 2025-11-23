d2dd55e61628e5744a4b15f943990e9584279ede
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-11-23 23:58:42 +0200

    add API controllers and authentication  for presentaion layer

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using Application.DataTransferObjects.User;
+using Application.ServiceInterfaces;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        public AuthenticationController(IServiceManager serviceManager )
+        {
+            _serviceManager = serviceManager;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var result = await _serviceManager.Authentication.RegisterUser(userForRegistration);
+            if (!result)
+                return BadRequest(ModelState);
+            
+            return StatusCode(201);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserForLoginDto userForAuthentication)
+        {
+            var result = await _serviceManager.Authentication.ValidateUser(userForAuthentication);
+            if (!result)
+                return Unauthorized();
+
+            var token = _serviceManager.Authentication.CreateToken();
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ Presentation/Controllers/PaymentsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        public PaymentsController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+        [HttpGet("{id}", Name = "PaymentById")]
+        public async Task<IActionResult> GetPaymentById(int id)
+        {
+            var payment = await _serviceManager.Payment.GetPaymentByBookingIdAsync(id, trackChanges: false);
+            return Ok(payment);
+        }
+    }
+}
