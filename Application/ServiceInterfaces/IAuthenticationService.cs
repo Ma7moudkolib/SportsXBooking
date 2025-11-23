b14ea7216c67e0c80fd36841fb304aaa36505dbb
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-11-23 23:45:58 +0200

    update service interfaces for application layer

---
+++ Application/ServiceInterfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DataTransferObjects.User;
+
+namespace Application.ServiceInterfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<bool> ValidateUser(UserForLoginDto userForLogin );
+        public string CreateToken();
+    }
+}
---
+++ Application/ServiceInterfaces/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using Application.DataTransferObjects.Payment;
+
+namespace Application.ServiceInterfaces
+{
+    public interface IPaymentService
+    {
+        Task<GetPaymentDto> GetPaymentByBookingIdAsync(int bookingId, bool trackChanges);
+    }
+}
