5f173c777f0be654216f05a769fc6d80e016aab9
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2025-12-06 12:54:03 +0200

    add globle service response and loging response

---
+++ Application/DataTransferObjects/User/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DataTransferObjects.User
+{
+    public record LoginResponse
+    {
+        public bool Suceeded { get; set; }
+        public string? Messenge { get; set; }
+        public string? Token { get; set; }
+    }
+}
